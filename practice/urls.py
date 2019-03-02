46f3b142a4d5dac49fc7dd8299d5fe030f7d64e7
fonzobalao <34019905+fonzobalao@users.noreply.github.com>
2019-03-02 12:19:37 +0800

    Added CRUD for blog posts. update and delete only for author of post

--- practice/urls.py
+++ practice/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from . import views
-
+from .views import (
+    PostListView, 
+    PostDetailView, 
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 urlpatterns = [
-    path('', views.home, name="practice-home"),
+    path('', PostListView.as_view(), name="practice-home"),
     path('about/', views.about, name="practice-about"),
+    path('post/<int:pk>', PostDetailView.as_view(), name="practice-detail"),
+    path('post/new', PostCreateView.as_view(), name="practice-create"), 
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="practice-update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="practice-delete"),
 ]
--- users/views.py
+++ users/views.py
@@ -36,4 +36,4 @@ def profile(request):
         'p_form': p_form,
     }
 
-    return render(request, 'users/profile.html', context)
+    return render(request, 'users/profile.html', context)        
